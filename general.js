0b65e3cf3a5978365c07eccfffd79f6a909f0273
scooller <scoollerx@hotmail.com>
2017-04-19 01:49:41 -0400

    Archivos

---
+++ general.js
@@ -0,0 +1,22 @@
+﻿function initialize() {
+	var options = {
+		types: ['(cities)'],
+		componentRestrictions: {country: "cl"}
+	};
+
+	var input = document.getElementById('city');
+	var autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+$(function() {
+	$("input#rut").rut({
+		formatOn: 'keyup',
+		minimumLength: 8
+	});
+	$("input#rut").rut().on('rutInvalido', function(e) {
+		$(this).parent('.form-group').removeClass('has-success');
+		$(this).parent('.form-group').addClass('has-error');
+	}).rut().on('rutValido', function(e, rut, dv) {
+		$(this).parent('.form-group').removeClass('has-error');
+		$(this).parent('.form-group').addClass('has-success');
+	});
+});
